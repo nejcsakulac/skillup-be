e1d4193a790dd4c4f8ee7b5c38ba65c27c1dbeb1
nejcsakulac <nejc.sakulac@gmail.com>
2025-04-14 18:35:14 +0200

    povezava do databasa, base in user entity, abstract service

---
+++ src/entities/base.entity.ts
@@ -0,0 +1,18 @@
+import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+import { Expose } from "class-transformer"
+import { IsUUID  } from "class-validator"
+
+export class Base{
+  @PrimaryGeneratedColumn('uuid')
+  @IsUUID()
+  @Expose()
+  id: string;
+
+  @CreateDateColumn()
+  @Expose()
+  created_at: Date
+
+  @UpdateDateColumn()
+  @Expose()
+  updated_at: Date
+}
